38c9f41672f65e26fa198c8dadcb5a06c6eaf63f
zel903 <98132420+zel903@users.noreply.github.com>
2022-05-15 22:57:42 -0500

    removed alert box

--- js/main.js
+++ js/main.js
@@ -3,9 +3,9 @@ function loadBG(){
     document.getElementById("paolasPicture").style.img="url(../images/profilePicStanding.jpg)";
 }
 window.onload=loadBG();
-
-var alerted = localStorage.getItem('alerted') || '';
-			if (alerted != 'yes') {
-			 alert("Welcome to Waxed Off! \r\nPlease take the time to look over our policies before booking.\r\nThank you!");
-			 localStorage.setItem('alerted','yes');
-			}
+//I can't view alert box to change it!
+// var alerted = localStorage.getItem('alerted') || '';
+// 			if (alerted != 'yes') {
+// 			 alert("Welcome to Waxed Off! \r\nPlease take the time to look over our policies before booking.\r\nThank you!");
+// 			 localStorage.setItem('alerted','yes');
+// 			}
