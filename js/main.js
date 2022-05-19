f4b2387cc9ed4a1f4ed2f82e7ce22e21b61e0cc3
zel903 <98132420+zel903@users.noreply.github.com>
2022-05-19 16:03:53 -0500

    pixels

--- js/main.js
+++ js/main.js
@@ -3,9 +3,17 @@ function loadBG(){
     document.getElementById("paolasPicture").style.img="url(../images/profilePicStanding.jpg)";
 }
 window.onload=loadBG();
+
+
+
+
+// idk what im doing fml 
+
+    
 //I can't view alert box to change it!
 // var alerted = localStorage.getItem('alerted') || '';
 // 			if (alerted != 'yes') {
 // 			 alert("Welcome to Waxed Off! \r\nPlease take the time to look over our policies before booking.\r\nThank you!");
 // 			 localStorage.setItem('alerted','yes');
 // 			}
+
